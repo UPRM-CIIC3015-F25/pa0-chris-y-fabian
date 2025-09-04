f6fcb1843a1f2e655a931d14b55bf2b47081886e
Chris0865 <christopher.gonzalez33@upr.edu>
2025-09-04 17:52:29 -0400

    Task 5 Merge Conflict

--- Game.py
+++ Game.py
@@ -12,7 +12,7 @@ def ball_movement():
 
     # Start the ball movement when the game begins
     # TODO Task 5 Create a Merge Conflict
-    speed = 7
+    speed = 10
     if start:
         ball_speed_x = speed * random.choice((1, -1))  # Randomize initial horizontal direction
         ball_speed_y = speed * random.choice((1, -1))  # Randomize initial vertical direction
@@ -95,7 +95,7 @@ start = False  # Indicates if the game has started
 while True:
     # Event handling
     # TODO Task 4: Add your name
-    name = "John Doe"
+    name = "Christopher Gonzalez"
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # Quit the game
             pygame.quit()
@@ -118,6 +118,7 @@ while True:
     player_movement()
 
     # Visuals
+    green = pygame.Color('green')
     light_grey = pygame.Color('grey83')
     red = pygame.Color('red')
     screen.fill(bg_color)  # Clear screen with background color
